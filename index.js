fd96917901acfed884b60dcd40fadf933641a9f9
chishuix <chishui777@gmail.com>
2025-04-19 14:00:17 +0800

    已通过

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer");
+const upload = multer({dest: 'uploads/'});
 
 var app = express();
 
@@ -11,8 +13,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+/*
+2. 您可以提交一个包含文件上传的表单。
+3. 表单文件输入字段设置了 name 属性为 upfile 。
+4. 当您提交文件时，您将在 JSON 响应中接收到文件 name ， type ，和 size 以字节为单位。
+ */
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  let responseMsg = ({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+  res.json(responseMsg);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
